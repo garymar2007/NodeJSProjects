744f742ce57afb3cb056c4b64b7ca783800887c5
Gary Ma <gary.ma@entelect.co.za>
2024-05-29 21:34:38 +0200

    added client-server communications

---
+++ client-server/server/server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    //console.log('request made');
+    //console.log(req);
+    console.log(req.url, req.method);
+
+    //set header content type
+    res.setHeader('Content-Type', 'text/html');
+
+    // res.write('<head><link rel="stylesheet" href="#"></head');
+    // res.write('<p>Hello, welcome to Node.js world!</p>');
+    // res.write('<h2>Hello, welcome to Node.js world!</h2>');
+
+    //send html file
+    fs.readFile('../views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end();
+        } else {
+            res.write(data);
+            res.end();
+        }
+    })
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for requests on port 3000...');
+});
