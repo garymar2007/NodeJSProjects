5657aa88deecc5eabb88965642733b8f193cd645
Gary Ma <gary.ma@entelect.co.za>
2024-06-02 12:46:50 +0200

    finished all HTTP actions

--- mongodb/app.js
+++ mongodb/app.js
@@ -103,6 +103,16 @@ app.post('/blogs', (req, res) => {
         });
 });
 
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then((result) => {
+            res.json({ redirect: '/blogs' })
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
 
 
 // app.get('/about-us', (req, res) => {
